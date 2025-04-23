5821b41814a1f8bb3a1f3cfb36a12aab7a7a2efd
Sheetal Naik <sheetalnaik848@gmail.com>
2025-04-23 14:44:41 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { AiOutlineArrowRight } from 'react-icons/ai'
 
-const Button = ({ text }) => {
+const Button = ({ text, bgcolor }) => {
   return (
     <>
-    <div className="button-container">
+    <div className="button-container" style={{ background: bgcolor }}>
         <a href="#">
-            <span className="button-text">{text}{" "}</span>
-            <span className='arrow-right'><AiOutlineArrowRight /></span>
+            <span className="button-text" style={{ color: bgcolor != "#000" ? "#000" : "#fff" }}>{text}{" "}</span>
+            <span className='arrow-right'style={{ color: bgcolor != "#000" ? "#000" : "#fff" }}><AiOutlineArrowRight /></span>
         </a>
     </div>
     </>
