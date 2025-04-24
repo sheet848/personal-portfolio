1fa9b5055bd7d2863a99dc8eab899f4e54725091
Sheetal Naik <sheetalnaik848@gmail.com>
2025-04-24 14:54:49 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
+import NavBar from './components/NavBar'
 import ProjectSection from './components/ProjectSection'
 import TechStack from './components/TechStack'
 
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <>
+    <NavBar />
     <main className='layout'>
       <Hero />
       <ProjectSection />
