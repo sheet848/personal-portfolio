9570b0bb5fe7616b3579500fd9d737f72e13bae6
Sheetal Naik <sheetalnaik848@gmail.com>
2025-05-13 22:49:05 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HiOutlineArrowSmallRight } from 'react-icons/hi2';
+//import { HiOutlineArrowSmallRight } from 'react-icons/hi2';
 
 const Button = ({ text, bgcolor }) => {
   return (
@@ -7,7 +7,6 @@ const Button = ({ text, bgcolor }) => {
     <div className="button-container" style={{ background: bgcolor }}>
         <a href="#">
             <span className="button-text" style={{ color: bgcolor != "#000" ? "#000" : "#fff" }}>{text}{" "}</span>
-            <span className='arrow-right'style={{ color: bgcolor != "#000" ? "#000" : "#fff" }}><HiOutlineArrowSmallRight /></span>
         </a>
     </div>
     </>
