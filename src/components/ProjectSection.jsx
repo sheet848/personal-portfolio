7baf53ca958518489ce6cef2f2c82e377e945ba7
Sheetal Naik <sheetalnaik848@gmail.com>
2025-05-14 15:01:33 +0530

    Update ProjectSection.jsx

--- src/components/ProjectSection.jsx
+++ src/components/ProjectSection.jsx
@@ -5,7 +5,7 @@ import { work } from '../constants'
 const ProjectSection = () => {
   return (
     <>
-    <div className="project-container">
+    <div className="project-container" id='work'>
         {
             work.map((item, index) => (
                 <Card key={index} card={item} />
