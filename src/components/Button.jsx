76ae752f03cfef6d11c9132c96c2b1e4e0be9ea0
Sheetal Naik <sheetalnaik848@gmail.com>
2025-05-14 21:42:26 +0530

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Button = ({ text, bgcolor }) => {
   return (
     <>
-      <a href="#">
+      <a href="mailto:sheetalnaik310@gmail.com" target="_blank">
         <div className="button-container" style={{ background: bgcolor }}>
           <span className="button-text" style={{ color: bgcolor != "#000" ? "#000" : "#fff" }}>{text}{" "}</span>
         </div>
