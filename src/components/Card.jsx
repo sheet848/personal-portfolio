6cb8fd3a5e45db9cc4e822b910674dafcef0338b
Sheetal Naik <sheetalnaik848@gmail.com>
2025-05-14 14:59:59 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,7 +6,9 @@ const Card = ({ card }) => {
     <div className="card-container">
       <div className="card">
         <div className="thumbnail">
+          <a href={card.preview} className="thumbnail-link" target='_blank'>
             <img className="img-thumbnail" src={card.img} alt="project-img" />
+          </a>
         </div>
         <p className='project-source'><a href={card.github} target="_blank">Github</a></p>
 
